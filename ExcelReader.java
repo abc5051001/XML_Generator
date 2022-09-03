970bd2845a8580d4b4d4954bae03c028301e2e08
Shawn Lai <82924798+abc5051001@users.noreply.github.com>
2022-09-02 20:20:08 -0400

    Update ExcelReader.java

--- ExcelReader.java
+++ ExcelReader.java
@@ -13,7 +13,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelReader {
     public  String path;
-    public  FileInputStream fis = null;
+    public  FileInputStream file = null;
     public  FileOutputStream fileOut =null;
     private XSSFWorkbook workbook = null;
     private XSSFSheet sheet = null;
@@ -24,37 +24,14 @@ public class ExcelReader {
        
         this.path=path;
         try {
-            fis = new FileInputStream(path);
-            workbook = new XSSFWorkbook(fis);
+            file = new FileInputStream(path);
+            workbook = new XSSFWorkbook(file);
             sheet = workbook.getSheetAt(0);
-            fis.close();
-        } catch (Exception e) {
+            file.close();
+        } 
+        catch (Exception e) {
             e.printStackTrace();
         }
-    }
-   
-   
-    // returns the row count in a specific column in the sheet
-    public int getRowCountOfaColumn(String sheetName, String colName) {
-        int index = workbook.getSheetIndex(sheetName);
-        int col_Num = -1;
-        sheet = workbook.getSheetAt(index);
-        row = sheet.getRow(0);
-        for (int i = 0; i < row.getLastCellNum(); i++) {
-            if (row.getCell(i).getStringCellValue().trim().equals(colName.trim()))
-                col_Num = i;
-        }
-        int number = 0;
-        int rowCount = sheet.getLastRowNum() + 1;
-        for (int i = 0; i <= rowCount; i++) {
-            if(sheet.getRow(i).getCell(col_Num) != null) {
-                number += 1;
-            }
-            else {
-                break;
-            }
-        }
-        return number;
     }
    
     // returns the row count in a sheet
@@ -93,65 +70,13 @@ public class ExcelReader {
             cell = row.getCell(col_Num);
             if (cell == null)
                 return "";
-            //System.out.println(cell.getCellTypeEnum());
             if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
-                //getCellTypeEnum
                 return cell.getStringCellValue();
             }
             else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC || cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
                 DataFormatter formatter = new DataFormatter();
                 String var_name = formatter.formatCellValue(cell);
                 String cellText = String.valueOf(var_name);
-                if (HSSFDateUtil.isCellDateFormatted(cell)) {
-                    double d = cell.getNumericCellValue();
-                    Calendar cal = Calendar.getInstance();
-                    cal.setTime(HSSFDateUtil.getJavaDate(d));
-                    cellText = (String.valueOf(cal.get(Calendar.YEAR))).substring(2);
-                    cellText = cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.MONTH) + 1 + "/" + cellText;
-                }
-                return cellText;
-            } else if (cell.getCellType() == Cell.CELL_TYPE_BLANK) {
-                return "";
-            }
-            else {
-                return String.valueOf(cell.getBooleanCellValue());
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            return "row " + rowNum + " or column " + colName + " does not exist in xls";
-        }
-    }
-   
-   
-    public String getCellDataFromFormula(String sheetName, String colName, int rowNum) {
-        try {
-            if (rowNum <= 0)
-                return "";
-            int index = workbook.getSheetIndex(sheetName);
-            int col_Num = -1;
-            if (index == -1)
-                return "";
-            sheet = workbook.getSheetAt(index);
-            row = sheet.getRow(0);
-            for (int i = 0; i < row.getLastCellNum(); i++) {
-                if (row.getCell(i).getStringCellValue().trim().equals(colName.trim()))
-                    col_Num = i;
-            }
-            if (col_Num == -1)
-                return "";
-            sheet = workbook.getSheetAt(index);
-            row = sheet.getRow(rowNum - 1);
-            if (row == null)
-                return "";
-            cell = row.getCell(col_Num);
-            if (cell == null)
-                return "";
-            if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
-                return cell.getStringCellValue();
-            }
-            else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC || cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
-                String cellText = String.valueOf(cell.getNumericCellValue());
                 if (HSSFDateUtil.isCellDateFormatted(cell)) {
                     double d = cell.getNumericCellValue();
                     Calendar cal = Calendar.getInstance();
